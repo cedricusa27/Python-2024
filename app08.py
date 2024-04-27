35018294381f229fe7ea0c9012c04e01ee4ae422
cedricusa27 <cedrickonan27@gmail.com>
2024-04-27 11:36:54 -0400

    updated files

--- app08.py
+++ app08.py
@@ -1,5 +1,5 @@
 secret_number = 3
-guess = int(input("Guess a nuumber"))
+guess = int(input("Guess a number"))
 while guess != secret_number:
     guess = int(input("Guess a number"))
 else: 
---
+++ app09.py
@@ -0,0 +1,33 @@
+# age1 = 24
+# age2 = 16
+# if("both ages are higher than 18" ): 
+#     print("You are bboth adults")
+# elif(age1 >= 18 or age2 >= 18):
+#   print("One of you is an adult")
+# else: 
+#   print("You are both children") 
+    
+    
+# is_hungry = True
+# if(not is_hungry):
+#   print("You are not hungry")
+# else:
+#   print("You are hungry")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Logic and Bit Operation 
+# Operators
+#
+#
